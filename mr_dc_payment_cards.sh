4e100a72086edcb91f976a618dbfca42cb637e4b
Danny Holloway <hollowaydp@gmail.com>
2014-07-29 23:17:58 -0400

    script to kick off hadoop job

---
+++ mr_dc_payment_cards.sh
@@ -0,0 +1,9 @@
+#cat input/purchase_card_transactions_csv.csv | python mapper.py | sort | python reducer.py
+
+rm -rf ./output
+
+hadoop jar /usr/local/Cellar/hadoop//2.4.1/libexec/share/hadoop/tools/lib/hadoop-streaming-2.4.1.jar \
+-file ./mapper.py -mapper mapper.py \
+-file ./reducer.py -reducer reducer.py \
+-input ./input/purchase_card_transactions_csv.csv \
+-output ./output
