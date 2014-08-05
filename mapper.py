682229a8963b679216a2c13e295f0baa7afbb29e
Danny Holloway <hollowaydp@gmail.com>
2014-08-04 21:52:01 -0400

    make mapper generic

--- mapper.py
+++ mapper.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
+from argparse import ArgumentParser
 from csv import reader
 import sys
 
-for parts in reader(sys.stdin):
-    try:
-        vendor_name = parts[3].strip()
-        transaction_amount = parts[2].strip()
-        print '%s\t%s' % (vendor_name, transaction_amount)
-    except:
-        pass
+
+def process(k, v, file):
+    for parts in reader(file):
+        try:
+            key = parts[k].strip()
+            value = parts[v].strip()
+            print '%s\t%s' % (key, value)
+        except:
+            pass
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Simple MapReduce mapper')
+    parser.add_argument('key', type=int)
+    parser.add_argument('value', type=int)
+    args = parser.parse_args()
+    process(args.key, args.value, sys.stdin)
--- mr_dc_payment_cards.sh
+++ mr_dc_payment_cards.sh
@@ -1,9 +1,9 @@
-#cat input/purchase_card_transactions_csv.csv | ./mapper.py | sort | ./reducer.py
+#cat input/purchase_card_transactions_csv.csv | ./mapper.py 3 2| sort | ./reducer.py
 
 rm -rf ./output
 
 hadoop jar /usr/local/Cellar/hadoop//2.4.1/libexec/share/hadoop/tools/lib/hadoop-streaming-2.4.1.jar \
--file ./mapper.py -mapper mapper.py \
+-file ./mapper.py -mapper 'mapper.py 3 2' \
 -file ./reducer.py -reducer reducer.py \
 -input ./input/purchase_card_transactions_csv.csv \
 -output ./output
